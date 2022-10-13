0dc72ec5d818b71b590496dbbf9e7fe2d9371799
DaweUrbi <urban.da@icloud.com>
2022-10-13 09:00:22 -0700

    days updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,100 +1,128 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./App.scss";
 
 import DayList from "./components/DayList";
 import Appointment from "./components/Appointment";
-import daysData from "./components/__mocks__/days.json";
-import appointmentsData from "./components/__mocks__/appointments.json";
+// import daysData from "./components/__mocks__/days.json";
+// import appointmentsData from "./components/__mocks__/appointments.json";
 
 export default function Application() {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState(daysData);
-  const [appointments, setAppointments] = useState(appointmentsData);
+	const [day, setDay] = useState("Monday");
+	const [days, setDays] = useState("");
+	const [appointments, setAppointments] = useState("");
 
-  function bookInterview(id, interview) {
-    console.log(id, interview);
-    const isEdit = appointments[id].interview;
+	useEffect(() => {
+    const urlDays = "http://localhost:8000/days";
+    const urlAppointments = "http://localhost:8000/appointments";
 
-    setAppointments((prev) => {
-      const appointment = {
-        ...prev[id],
-        interview: { ...interview },
-      };
-      const appointments = {
-        ...prev,
-        [id]: appointment,
-      };
-      return appointments;
-    });
+    const fetchDays = async () => {
+      try {
+          const response = await fetch(urlDays);
+          const json = await response.json();
+          console.log(json);
+          setDays(json);
+      } catch (error) {
+          console.log("error", error);
+      }
+    };
+    fetchDays();
 
-    if (!isEdit) {
-      setDays((prev) => {
-        const updatedDay = {
-          ...prev[day],
-          spots: prev[day].spots - 1,
-        };
-        const days = {
-          ...prev,
-          [day]: updatedDay,
-        };
-        return days;
-      });
-    }
-  }
+    const fetchAppointments = async () => {
+      try {
+          const response = await fetch(urlAppointments);
+          const json = await response.json();
+          console.log(json);
+          setAppointments(json);
+      } catch (error) {
+          console.log("error", error);
+      }
+    };
+    fetchAppointments();
+	}, []);
 
-  function cancelInterview(id) {
-    setAppointments((prev) => {
-      const updatedAppointment = {
-        ...prev[id],
-        interview: null,
-      };
-      const appointments = {
-        ...prev,
-        [id]: updatedAppointment,
-      };
-      return appointments;
-    });
+	function bookInterview(id, interview) {
+		console.log(id, interview);
+		const isEdit = appointments[id].interview;
 
-    setDays((prev) => {
-      const updatedDay = {
-        ...prev[day],
-        spots: prev[day].spots + 1,
-      };
-      const days = {
-        ...prev,
-        [day]: updatedDay,
-      };
-      return days;
-    });
-    
-  }
-  return (
-    <main className="layout">
-      <section className="sidebar">
-        <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
-        />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
-        </nav>
-      </section>
-      <section className="schedule">
-        {Object.values(appointments).map((appointment) => (
-          <Appointment
-            key={appointment.id}
-            {...appointment}
-            bookInterview={(interview) =>
-              bookInterview(appointment.id, interview)
-            }
-            cancelInterview={cancelInterview}
-          />
-        ))}
-        <Appointment key="last" time="5pm" />
-      </section>
-    </main>
-  );
+		setAppointments((prev) => {
+			const appointment = {
+				...prev[id],
+				interview: { ...interview },
+			};
+			const appointments = {
+				...prev,
+				[id]: appointment,
+			};
+			return appointments;
+		});
+
+		if (!isEdit) {
+			setDays((prev) => {
+				const updatedDay = {
+					...prev[day],
+					spots: prev[day].spots - 1,
+				};
+				const days = {
+					...prev,
+					[day]: updatedDay,
+				};
+				return days;
+			});
+		}
+	}
+
+	function cancelInterview(id) {
+		setAppointments((prev) => {
+			const updatedAppointment = {
+				...prev[id],
+				interview: null,
+			};
+			const appointments = {
+				...prev,
+				[id]: updatedAppointment,
+			};
+			return appointments;
+		});
+
+		setDays((prev) => {
+			const updatedDay = {
+				...prev[day],
+				spots: prev[day].spots + 1,
+			};
+			const days = {
+				...prev,
+				[day]: updatedDay,
+			};
+			return days;
+		});
+	}
+	return (
+		<main className="layout">
+			<section className="sidebar">
+				<img
+					className="sidebar--centered"
+					src="images/logo.png"
+					alt="Interview Scheduler"
+				/>
+				<hr className="sidebar__separator sidebar--centered" />
+				<nav className="sidebar__menu">
+					<DayList days={days} value={day} onChange={setDay} />
+				</nav>
+			</section>
+			<section className="schedule">
+				{Object.values(appointments).map((appointment) => (
+					<Appointment
+						key={appointment.id}
+						{...appointment}
+						bookInterview={(interview) =>
+							bookInterview(appointment.id, interview)
+						}
+						cancelInterview={cancelInterview}
+					/>
+				))}
+				<Appointment key="last" time="5pm" />
+			</section>
+		</main>
+	);
 }
